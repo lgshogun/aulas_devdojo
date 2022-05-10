0b7aa91574ad452ca90641026571b2fca7f73616
LGShogun <lauro.gpereira@gmail.com>
2022-05-10 20:59:32 -0300

    Aula 02-10

---
+++ Semana1/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    // comentario foda
+
+    /*U
+    A
+    U*/
+
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
---
+++ Semana1/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Arrays;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        System.out.println("A idade e "+idade+" anos");
+    }
+}
